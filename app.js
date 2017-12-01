20f9eb7f2170199a5da423de2c276eb70efcc635
Carlos <carlosrecinos1999@gmail.com>
2017-12-01 00:05:15 -0600

    delete reparado

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.listen(process.env.PORT || 5000,(req,res)=>{
+app.listen(process.env.PORT || 3005,(req,res)=>{
     console.log('Servidor corriendo en puerto 3005');
 });
 
@@ -23,7 +23,7 @@ app.put('/tareas/:idTarea',middleware.isAuth,ControlTarea.modificarTarea);
 app.delete('/tareas/:idTarea',ControlTarea.eliminarTarea);
 app.post('/registrar',autenticacion.registrar);
 app.put('/tareas/finalizar/:idTarea',ControlTarea.finalizarTarea);
-app.get('/login',(req,res)=>{
+app.post('/login',autenticacion.ingresar,(req,res)=>{
     res.send({mensaje:"Acceso permitido",usuario : req.payload});
 });
 

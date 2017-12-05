14f0a13b2f8e86f6096f4f67e1c84214b8e694ec
Carlos <carlosrecinos1999@gmail.com>
2017-12-05 14:32:12 -0600

    Actualizacion

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.get('/',(req,res)=>{
 
 app.use(function(req,res,next){
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
